54363e98b6d5826b678758063aa28b1ce670ca2c
DBeumans <buncho.gamingnetwork@gmail.com>
2017-06-21 14:32:09 +0200

    added shooting and bullet script

--- public/Assets/_Scripts/classes/MouseInput.js
+++ public/Assets/_Scripts/classes/MouseInput.js
@@ -3,12 +3,17 @@ class MouseInput {
   {
     this.mouseX = 0;
     this.mouseY = 0;
+    this.mouseClicked = false;
+    this.mouseDown = false;
 
     this.element = element || window; // reference to the canvas for the mouse track.
 
     this.element.addEventListener("mousemove", this.OnMouseMove.bind(this));
     this.element.addEventListener("click", this.OnMouseClick.bind(this));
-    this.element.addEventListener("mouseout", this.OnMouseOut);
+    this.element.addEventListener("mouseout", this.OnMouseOut.bind(this));
+    this.element.addEventListener("mouseup", this.OnMouseUp.bind(this));
+    this.element.addEventListener("mousedown", this.OnMouseDown.bind(this));
+
   }
 
   OnMouseMove(e) {
@@ -17,13 +22,25 @@ class MouseInput {
 
   }
 
-  OnMouseClick(e)
+  OnMouseDown(e)
   {
-
+    this.mouseDown = true;
   }
 
-  onMouseOut(e) {
-    	console.log("out");
+  OnMouseClick(e)
+  {
+    this.mouseClicked = true;
+  }
+
+  OnMouseUp(e)
+  {
+    this.mouseClicked = false;
+    this.mouseDown = false;
+  }
+
+  OnMouseOut(e)
+  {
+
   }
 
 }
